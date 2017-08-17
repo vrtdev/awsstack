3431a2a2f8bc78ba4160c734323685c187d9f1a3
Stefan - Zipkid - Goethals <stefan.goethals@vrt.be>
2017-08-17 15:45:29 +0200

    Skel for template file handling

---
+++ lib/awsstack/cfntemplate.rb
@@ -0,0 +1,9 @@
+require 'aws-sdk'
+
+module AwsStack
+  # AWS CFN template file handling
+  class CfnTemplate
+    def initialize(options)
+    end
+  end
+end
--- lib/awsstack/cloudformation.rb
+++ lib/awsstack/cloudformation.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'awsstack/cfntemplate'
 
 module AwsStack
   # AWS Session creation with profile
